3cf2088dc1e05e014e3cf601ce5966dc83a8128c
Almer Gutierrez <almergutierrez@MacBookPro.cpe.tricom.net>
2026-07-31 19:59:37 -0400

    feat: crud de medicamentos

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ app.get('/', (req, res) => {
 });
 
 const glucosaRoutes = require('./routes/glucosa');
+const medicamentosRoutes = require('./routes/medicamentos');
+
 app.use('/glucosa', glucosaRoutes);
+app.use('/medicamentos', medicamentosRoutes);
 
 app.listen(PORT, () => {
   console.log('Servidor corriendo en el puerto ' + PORT);
